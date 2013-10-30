be7cd73ba814c341dc79ea6d9bdb9c99f4089189
Jakub Suchecki <b00045505@student.itb.ie>
2013-10-30 14:01:27 +0000

    test code

---
+++ DiseaseAutomaton/src/Test.cpp
@@ -0,0 +1,21 @@
+/*
+ * Test.cpp
+ *
+ *  Created on: 30 Oct 2013
+ *      Author: kuba
+ */
+
+#include "Test.h"
+
+Test::Test() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Test::~Test() {
+	// TODO Auto-generated destructor stub
+}
+
+int main(){
+	return 0;
+}
