d484845bba027e52e0ce6b6bd963fb3621c53aca
Jakub Suchecki <b00045505@student.itb.ie>
2013-12-03 21:21:53 +0000

    first version, only the data structure and printing of the data structure containing the grid with cells and people inside of cells and their health state.

--- DiseaseAutomaton/Debug/src/subdir.mk
+++ DiseaseAutomaton/Debug/src/subdir.mk
@@ -4,20 +4,32 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/Test.cpp 
+../src/Cell.cpp \
+../src/Cell_test.cpp \
+../src/Person.cpp \
+../src/Person_test.cpp \
+../src/main.cpp 
 
 OBJS += \
-./src/Test.o 
+./src/Cell.o \
+./src/Cell_test.o \
+./src/Person.o \
+./src/Person_test.o \
+./src/main.o 
 
 CPP_DEPS += \
-./src/Test.d 
+./src/Cell.d \
+./src/Cell_test.d \
+./src/Person.d \
+./src/Person_test.d \
+./src/main.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../usr/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
